74e85016e530a1cc53766defa5061ebbc521007b
Sharath <sharathnair9999@gmail.com>
2022-02-01 11:57:34 +0530

    initial commit with skeleton of library

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+const hamburger = document.querySelector(".ham");
+const aside = document.querySelector("nav");
+const navSection = document.querySelector(".collapsible");
+const navLinks = document.querySelectorAll(".nav-links");
+const close = document.querySelector(".close");
+
+document.addEventListener(
+  "click",
+  () => {
+    if (aside.classList.contains("nav-open")) {
+      aside.classList.remove("nav-open");
+    }
+  },
+  true
+);
+
+close.addEventListener("click", () => {
+  aside.classList.remove("nav-open");
+});
+
+hamburger.addEventListener("click", () => {
+  aside.classList.add("nav-open");
+});
+
+navLinks.forEach((navLink) =>
+  navLink.addEventListener("click", (e) => {
+    console.log(e.target);
+    let activeLink = document.querySelector(".nav-link-active");
+    activeLink && activeLink.classList.remove("nav-link-active");
+    e.target.classList.add("nav-link-active");
+  })
+);
